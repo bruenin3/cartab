bf7aa7a80df8faea189996a6d49929f204fa7a1d
tbarrettwilsdon <tbarrettwilsdon@gmail.com>
2014-03-20 21:51:33 -0700

    Updated
    Updated core functionality

--- index.php
+++ index.php
@@ -1,127 +1,68 @@
-<!DOCTYPE html>
-<html><head> 
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head> 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, height=device-height, target-densitydpi=device-dpi"/>
-<title>CarTab</title> 
-</head>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script src="http://malsup.github.com/jquery.form.js"></script> 
+<script type="text/javascript" charset="utf-8" src="getlocation.js"></script>
+<script type="text/javascript" src="http://fgnass.github.com/spin.js/spin.js"></script>
+<title>CarTab</title> 
+</head>
 <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="style.css">
-<body style="font-family: 'Montserrat', sans-serif; font-size: 13px; color: white;"><div id="container" align="center">
-<p id="header">cartab</p>
-<script type="text/javascript" charset="utf-8">
-   function getLocation(){
-      console.log("Entering getLocation()");
-      if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(
-      displayCurrentLocation,
-      displayError,
-      { 
-        maximumAge: 3000, 
-        timeout: 5000, 
-        enableHighAccuracy: true 
-      });
-    }else{
-      console.log("No geolocation support");
-    } 
-      console.log("Exiting getLocation()");
-    };
-    function displayCurrentLocation(position){
-      var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    getAddressFromLatLang(latitude,longitude);
-    }
-   function  displayError(error){
-    var errorType = {
-      0: "Unknown error",
-      1: "Permission denied by user",
-      2: "Position is not available",
-      3: "Request time out"
-    };
-    var errorMessage = errorType[error.code];
-    if(error.code == 0  || error.code == 2){
-      errorMessage = errorMessage + "  " + error.message;
-    }
-    alert("Error Message " + errorMessage);
-  }
-    function getAddressFromLatLang(lat,lng){
-      var geocoder = new google.maps.Geocoder();
-        var latLng = new google.maps.LatLng(lat, lng);
-        geocoder.geocode( { 'latLng': latLng}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          if (results[1]) {
-            console.log(results[1]);
-			var form = document.getElementById('Address');
-            form.elements.start.value = (results[1].formatted_address);
-          }
-        }else{
-          alert("Geocode was not successful for the following reason: " + status);
-        }
-        });
-    }
-    </script>
-  </head>
-  <body>
-
+<script type="text/javascript">		
+$.fn.spin = function(opts) {
+			this.each(function() {
+				var $this = $(this),
+					spinner = $this.data('spinner');
+ 
+				if (spinner) spinner.stop();
+				if (opts !== false) {
+				  opts = $.extend({color: $this.css('color')}, opts);
+				  spinner = new Spinner(opts).spin(this);
+				  $this.data('spinner', spinner);
+				}
+			});
+			return this;
+		};
+		$(function() {
+	$(".Button").click(function(e) {
+				var opts = {
+				  lines: 12, 
+				  length: 7, 
+				  width: 5, 
+				  radius: 10, 
+				  color: '#fff', 
+				  speed: 1, 
+				  trail: 66,
+				  shadow: true 
+				};
+								$("#spin").show().spin(opts);
+});
+});
+</script>
+<body style="font-family: 'Montserrat', sans-serif; font-size: 13px; color: white;">
+<div id="body">
+<div id="top">
+  <div id="top"><p id="toploc">Current Location: San Francisco, CA</p></div><div id="spin" style="display:none;"></div><p id="header">cartab</p>
   <input type="button" class="Button" id="getLocation" onclick="getLocation()" value="find me"/>
-    <form action="index.php" method="post" id="Address">
-      <p>
-        Start Address: 
+  <form action="index.php" method="post" id="Address">
+    <p>start address: 
         <input type="text" name="start" id="start">
         <br>
-        End Address:
+        end address:
         <input type="text" name="end" id="end">
   <br><br>
         <input type="submit" class="Button" name="submit" id="submit" value="submit">
-      </p>
+    </p>
 </form>
-<div id="map" style="width: 250px; height: 200px; font-family: Montserrat, sans-serif;"></div> 
+<div id="map" style="display:none; width: 250px; height: 200px; font-family: Montserrat, sans-serif;"></div> 
 <br><div id="duration"></div> 
 <div id="distance"></div> 
 <div id="distance2"></div> 
 <div id="distance3"></div>
-<script type='text/javascript'>
-    $("#Address").submit(function(event) {
-      event.preventDefault();
-      var $form = $( this ),
-          url = $form.attr( 'action' );
-      var posting = $.post( url, { start: $('#start').val(), end: $('#end').val() } );
-var start = $('#start').val();
-var end = $('#end').val();
-posting.done(function( data ) {
-   var directionsService = new google.maps.DirectionsService();
-   var directionsDisplay = new google.maps.DirectionsRenderer();
-   var myOptions = {
-     zoom:7,
-     mapTypeId: google.maps.MapTypeId.ROADMAP
-   }
-   var map = new google.maps.Map(document.getElementById("map"), myOptions);
-   directionsDisplay.setMap(map);
-   var request = {
-       origin: start,
-       destination: end,
-       travelMode: google.maps.DirectionsTravelMode.DRIVING
-   };
-   directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-         document.getElementById('distance').innerHTML = 
-           "Uber cost: $" + Math.round( (3+(response.routes[0].legs[0].distance.value*0.000621371*1.5)+(response.routes[0].legs[0].duration.value/60*.30)) *100 ) /100;
-         document.getElementById('distance2').innerHTML = 
-           "Lyft cost: $" + Math.round( (3.5+(response.routes[0].legs[0].distance.value*0.000621371*1.90)+(response.routes[0].legs[0].duration.value/60*.40)) *100 ) /100;
-         document.getElementById('distance3').innerHTML = 
-           "Cab cost: $" + Math.round( (3.5+(response.routes[0].legs[0].distance.value*0.000621371*2.75)+(response.routes[0].legs[0].duration.value/60*.55)) *100 ) /100;
-         document.getElementById('duration').innerHTML = 
-            "Time: " + Math.round( (response.routes[0].legs[0].duration.value/60) *100 ) / 100 + " minutes";
-         directionsDisplay.setDirections(response);
-      }
-   });
-  });
-    });
-</script>
-<script type="text/javascript">
-   </script> <br><br>
-</div>
+<script type='text/javascript' src="address.js"></script></div></div>
 </body> 
+<script type="text/javascript" src="analytics.js"></script>
 </html>
