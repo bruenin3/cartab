0558c78fc54c747c082a92e3cb3f599f425405ca
tbarrettwilsdon <tbarrettwilsdon@gmail.com>
2014-11-29 14:03:24 -0800

    Delete adlogin.php

--- adlogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connect = ldap_connect("dc01.yelp.com");
-if ($bind = ldap_bind($connect, $_POST['username'], $_POST['password'])) {
-echo "You've successfully logged in";
-} else {
-echo "Sorry, your password is incorrect."
-}
-?>
